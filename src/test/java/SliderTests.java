6438766c2fda83ec80935f7b208e8818b0ba5932
Valeria Karandasheva <karandasheva.valeria@yandex.ru>
2024-07-31 00:38:25 +0300

    small fixes added

--- src/test/java/SliderTests.java
+++ src/test/java/SliderTests.java
@@ -1,40 +1,40 @@
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import pagesobject.MainPage;
+    import org.junit.Test;
+    import org.junit.runner.RunWith;
+    import org.junit.runners.Parameterized;
+    import pagesobject.MainPage;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+    import static org.hamcrest.CoreMatchers.equalTo;
+    import static org.hamcrest.MatcherAssert.assertThat;
 
-@RunWith(Parameterized.class)
-public class SliderTests extends BaseTest {
-    private String headerText;
-    private String textToCompare;
+    @RunWith(Parameterized.class)
+    public class SliderTests extends BaseTest {
+        private String headerText;
+        private String textToCompare;
 
-    @Parameterized.Parameters
-    public static Object[][] checkSlidersData() {
-        return new Object[][]{
-                {"Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
-                {"Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
-                {"Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
-                {"Можно ли заказать самокат прямо на сегодня?", "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
-                {"Можно ли продлить заказ или вернуть самокат раньше?", "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
-                {"Вы привозите зарядку вместе с самокатом?", "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
-                {"Можно ли отменить заказ?", "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
-                {"Я жизу за МКАДом, привезёте?", "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
-        };
+        @Parameterized.Parameters
+        public static Object[][] checkSlidersData() {
+            return new Object[][]{
+                    {"Сколько это стоит? И как оплатить?", "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                    {"Хочу сразу несколько самокатов! Так можно?", "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                    {"Как рассчитывается время аренды?", "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                    {"Можно ли заказать самокат прямо на сегодня?", "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                    {"Можно ли продлить заказ или вернуть самокат раньше?", "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                    {"Вы привозите зарядку вместе с самокатом?", "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                    {"Можно ли отменить заказ?", "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                    {"Я жизу за МКАДом, привезёте?", "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
+            };
+        }
+
+        public SliderTests(String headerText, String textToCompare) {
+            this.headerText = headerText;
+            this.textToCompare = textToCompare;
+        }
+
+        @Test
+        public void faqHasSlideresOpenTest() {
+            MainPage objMainPage = new MainPage(driver, headerText, textToCompare);
+            objMainPage.agreeToCookieUsage();
+            String accordeonText = objMainPage.openAccordeonAndGetText();
+            assertThat(accordeonText, equalTo(textToCompare));
+        }
     }
-
-    public SliderTests(String headerText, String textToCompare) {
-        this.headerText = headerText;
-        this.textToCompare = textToCompare;
-    }
-
-    @Test
-    public void faqHasSlideresOpenTest() {
-        MainPage objMainPage = new MainPage(driver, headerText, textToCompare);
-        objMainPage.agreeToCookieUsage();
-        String accordeonText = objMainPage.openAccordeonAndGetText();
-        assertThat(accordeonText, equalTo(textToCompare));
-    }
-}
